4c2eb6cce4322946a29feafd9efc3f71a6200ad7
Dompatchi <jens.scheidmann@gmail.com>
2019-08-21 17:09:06 +0200

    woopeeedohoo

--- src/main/java/ch/noseryoung/demo/Pokemon.java
+++ src/main/java/ch/noseryoung/demo/Pokemon.java
@@ -1,84 +1,166 @@
 package ch.noseryoung.demo;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Pokemon {
 	private Long id;
 	private String name;
-	private ArrayList<String> typing;
-	private HashMap<String, Integer> stats;
+	private ArrayList<Typing> type;
+	private Integer health;
+	private Integer attack;
+	private Integer defense;
+	private Integer specialAttack;
+	private Integer specialDefense;
+	private Integer speed;
+	private ArrayList<Ability> abilities;
+	
+	public Pokemon() {}
 	
 	/**
 	 * @param id
 	 * @param name
-	 * @param typing
-	 * @param stats
+	 * @param type
+	 * @param health
+	 * @param attack
+	 * @param defense
+	 * @param specialAttack
+	 * @param specialDefense
+	 * @param speed
+	 * @param abilities
 	 */
-	public Pokemon(Long id, String name, ArrayList<String> typing, HashMap<String, Integer> stats) {
+	public Pokemon(Long id, String name, ArrayList<Typing> type, Integer health, Integer attack, Integer defense,
+			Integer specialAttack, Integer specialDefense, Integer speed, ArrayList<Ability> abilities) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.typing = typing;
-		this.stats = stats;
+		this.type = type;
+		this.health = health;
+		this.attack = attack;
+		this.defense = defense;
+		this.specialAttack = specialAttack;
+		this.specialDefense = specialDefense;
+		this.speed = speed;
+		this.abilities = abilities;
 	}
-
+	
 	/**
 	 * @return the id
 	 */
 	public Long getId() {
 		return id;
 	}
-
 	/**
 	 * @param id the id to set
 	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	/**
 	 * @return the name
 	 */
 	public String getName() {
 		return name;
 	}
-
 	/**
 	 * @param name the name to set
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	/**
-	 * @return the typing
+	 * @return the type
 	 */
-	public ArrayList<String> getTyping() {
-		return typing;
+	public ArrayList<Typing> getType() {
+		return type;
 	}
-
 	/**
-	 * @param typing the typing to set
+	 * @param type the type to set
 	 */
-	public void setTyping(ArrayList<String> typing) {
-		this.typing = typing;
+	public void setType(ArrayList<Typing> type) {
+		this.type = type;
 	}
-
 	/**
-	 * @return the stats
+	 * @return the health
 	 */
-	public HashMap<String, Integer> getStats() {
-		return stats;
+	public Integer getHealth() {
+		return health;
 	}
-
 	/**
-	 * @param stats the stats to set
+	 * @param health the health to set
 	 */
-	public void setStats(HashMap<String, Integer> stats) {
-		this.stats = stats;
+	public void setHealth(Integer health) {
+		this.health = health;
 	}
-	
-	
-	
+	/**
+	 * @return the attack
+	 */
+	public Integer getAttack() {
+		return attack;
+	}
+	/**
+	 * @param attack the attack to set
+	 */
+	public void setAttack(Integer attack) {
+		this.attack = attack;
+	}
+	/**
+	 * @return the defense
+	 */
+	public Integer getDefense() {
+		return defense;
+	}
+	/**
+	 * @param defense the defense to set
+	 */
+	public void setDefense(Integer defense) {
+		this.defense = defense;
+	}
+	/**
+	 * @return the specialAttack
+	 */
+	public Integer getSpecialAttack() {
+		return specialAttack;
+	}
+	/**
+	 * @param specialAttack the specialAttack to set
+	 */
+	public void setSpecialAttack(Integer specialAttack) {
+		this.specialAttack = specialAttack;
+	}
+	/**
+	 * @return the specialDefense
+	 */
+	public Integer getSpecialDefense() {
+		return specialDefense;
+	}
+	/**
+	 * @param specialDefense the specialDefense to set
+	 */
+	public void setSpecialDefense(Integer specialDefense) {
+		this.specialDefense = specialDefense;
+	}
+	/**
+	 * @return the speed
+	 */
+	public Integer getSpeed() {
+		return speed;
+	}
+	/**
+	 * @param speed the speed to set
+	 */
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	/**
+	 * @return the abilities
+	 */
+	public ArrayList<Ability> getAbilities() {
+		return abilities;
+	}
+	/**
+	 * @param abilities the abilities to set
+	 */
+	public void setAbilities(ArrayList<Ability> abilities) {
+		this.abilities = abilities;
+	}
 }
--- src/main/java/ch/noseryoung/demo/PokemonController.java
+++ src/main/java/ch/noseryoung/demo/PokemonController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,19 +17,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pokemon")
 public class PokemonController {
 	
-	@GetMapping({"/{id}"})
+	@GetMapping("/{id}")
 	public @ResponseBody ResponseEntity<Pokemon> getPokemon(@PathVariable Long id) {
-		ArrayList<String> typing = new ArrayList<>();
-		typing.add("Grass");
+		ArrayList<Typing> resistantAgainst = new ArrayList<>();
+		ArrayList<Typing> weakAgainst = new ArrayList<>();
+		ArrayList<Typing> strongAgainst = new ArrayList<>();
+		Typing typing = new Typing("Fighting", resistantAgainst, weakAgainst, strongAgainst);
+		ArrayList<Typing> typings = new ArrayList<>();
+		typings.add(typing);
 		
-		HashMap<String, Integer> stats = new HashMap<>();
-		stats.put("Health", 75);
-		stats.put("Attack", 86);
-		stats.put("Defense", 67);
-		stats.put("Special Attack", 106);
-		stats.put("Special Defense", 67);
-		stats.put("Speed", 60);
+		Ability innerFocus = new Ability("Inner focus", "This pokemon cannot flinch.");
+		Ability regenerator;
+		Ability reckless;
+		ArrayList<Ability> abilities = new ArrayList<>();
+		abilities.add(innerFocus);
 		
-		return new ResponseEntity<Pokemon>(new Pokemon(id, "Maractus", typing, stats), HttpStatus.OK);
+		
+		Pokemon pokemon = new Pokemon();
+		pokemon.setId(id);
+		pokemon.setHealth(65);
+		pokemon.setAttack(125);
+		pokemon.setDefense(60);
+		pokemon.setSpecialAttack(95);
+		pokemon.setSpecialDefense(60);
+		pokemon.setSpeed(105);
+		pokemon.setAbilities(abilities);
+		pokemon.setType(typings);
+		
+		return new ResponseEntity<Pokemon>(pokemon, HttpStatus.OK);
+	}
+	
+	@PostMapping("/")
+	public @ResponseBody ResponseEntity<String> createPokemon(@RequestBody Pokemon pokemon) {
+		return new ResponseEntity<String>(pokemon.getName() + " has been created ^^", HttpStatus.OK);
 	}
 }
