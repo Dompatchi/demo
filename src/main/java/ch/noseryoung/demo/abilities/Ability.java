68333b4d512601064909a7950a4d531e3c16d286
Jens H. S <jens.scheidmann@gmail.com>
2019-08-22 15:18:10 +0200

    oof

--- src/main/java/ch/noseryoung/demo/abilities/Ability.java
+++ src/main/java/ch/noseryoung/demo/abilities/Ability.java
@@ -1,8 +1,22 @@
 package ch.noseryoung.demo.abilities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Ability")
 public class Ability {
+	
+	@Id
+	@Column(name = "id")
 	private Long id;
+	
+	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "description")
 	private String description;
 	
 	public Ability(Long id, String name, String description) {
---
+++ src/main/java/ch/noseryoung/demo/abilities/AbilityService.java
@@ -0,0 +1,30 @@
+package ch.noseryoung.demo.abilities;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AbilityService {
+
+	public Ability getAbility(Long id) {
+		return null;
+	}
+
+	public ArrayList<Ability> getAllAbilities() {
+		return null;
+	}
+
+	public String createAbility(Ability ability) {
+		return null;
+	}
+
+	public String updateAbility(Ability ability) {
+		return null;
+	}
+
+	public String deleteAbility(Long id) {
+		return null;
+	}
+	
+}
