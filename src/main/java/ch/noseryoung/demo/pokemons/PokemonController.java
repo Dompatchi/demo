dc68e107e804c711ae71163ad908c19f29a3f040
Dompatchi <jens.scheidmann@gmail.com>
2019-08-22 11:37:50 +0200

    Services galore

--- src/main/java/ch/noseryoung/demo/pokemons/PokemonController.java
+++ src/main/java/ch/noseryoung/demo/pokemons/PokemonController.java
@@ -2,6 +2,7 @@ package ch.noseryoung.demo.pokemons;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -27,6 +28,13 @@ import ch.noseryoung.demo.typings.Typing;
 @RequestMapping("/pokemon")
 public class PokemonController {
 	
+	PokemonService pokemonService;
+	
+	@Autowired
+	public PokemonController(PokemonService pokemonService) {
+		this.pokemonService = pokemonService;
+	}
+	
 	/** This method returns a specific pokemon
 	 * 
 	 * @param id
@@ -34,32 +42,7 @@ public class PokemonController {
 	 */
 	@GetMapping("/{id}")
 	public @ResponseBody ResponseEntity<Pokemon> getPokemon(@PathVariable Long id) {
-		ArrayList<Typing> resistantAgainst = new ArrayList<>();		
-		ArrayList<Typing> weakAgainst = new ArrayList<>();
-		ArrayList<Typing> strongAgainst = new ArrayList<>();
-		Typing typing = new Typing("Fighting", resistantAgainst, weakAgainst, strongAgainst);
-		ArrayList<Typing> typings = new ArrayList<>();
-		typings.add(typing);
-		
-		Ability innerFocus = new Ability("Inner focus", "This pokemon cannot flinch.");
-		Ability regenerator = new Ability("Regenerator", "Heals a bit when switched out.");
-		Ability reckless = new Ability("Reckless", "Moves that have recoil or crash damage deal more damage.");
-		ArrayList<Ability> abilities = new ArrayList<>();
-		abilities.add(innerFocus);
-		abilities.add(regenerator);
-		abilities.add(reckless);
-				
-		Pokemon pokemon = new Pokemon();
-		pokemon.setId(id);
-		pokemon.setHealth(65);
-		pokemon.setAttack(125);
-		pokemon.setDefense(60);
-		pokemon.setSpecialAttack(95);
-		pokemon.setSpecialDefense(60);
-		pokemon.setSpeed(105);
-		pokemon.setAbilities(abilities);
-		pokemon.setType(typings);
-		
+		Pokemon pokemon = pokemonService.getPokemon(id);
 		return new ResponseEntity<Pokemon>(pokemon, HttpStatus.OK);
 	}
 	
@@ -69,49 +52,7 @@ public class PokemonController {
 	 */
 	@GetMapping({"", "/"})
 	public @ResponseBody ResponseEntity<ArrayList<Pokemon>> getAllPokemon() {
-		ArrayList<Typing> resistantAgainst = new ArrayList<>();
-		ArrayList<Typing> weakAgainst = new ArrayList<>();
-		ArrayList<Typing> strongAgainst = new ArrayList<>();
-		Typing typing = new Typing("Fighting", resistantAgainst, weakAgainst, strongAgainst);
-		ArrayList<Typing> typings = new ArrayList<>();
-		typings.add(typing);
-		
-		Ability innerFocus = new Ability("Inner focus", "This pokemon cannot flinch.");
-		Ability regenerator = new Ability("Regenerator", "Heals a bit when switched out.");
-		Ability reckless = new Ability("Reckless", "Moves that have recoil or crash damage deal more damage.");
-		ArrayList<Ability> abilities = new ArrayList<>();
-		abilities.add(innerFocus);
-		abilities.add(regenerator);
-		abilities.add(reckless);
-		
-		
-		Pokemon mienshao = new Pokemon();
-		mienshao.setId(Long.parseLong("620"));
-		mienshao.setHealth(65);
-		mienshao.setAttack(125);
-		mienshao.setDefense(60);
-		mienshao.setSpecialAttack(95);
-		mienshao.setSpecialDefense(60);
-		mienshao.setSpeed(105);
-		mienshao.setAbilities(abilities);
-		mienshao.setType(typings);
-		
-		Pokemon mienfoo = new Pokemon();
-		mienfoo.setId(Long.parseLong("619"));
-		mienfoo.setHealth(45);
-		mienfoo.setAttack(85);
-		mienfoo.setDefense(50);
-		mienfoo.setSpecialAttack(55);
-		mienfoo.setSpecialDefense(50);
-		mienfoo.setSpeed(65);
-		mienfoo.setAbilities(abilities);
-		mienfoo.setType(typings);
-		
-		
-		ArrayList<Pokemon> pokemons = new ArrayList<>();
-		pokemons.add(mienfoo);
-		pokemons.add(mienshao);
-		
+		ArrayList<Pokemon> pokemons = pokemonService.getAllPokemons();
 		return new ResponseEntity<ArrayList<Pokemon>>(pokemons, HttpStatus.OK);
 	}
 	
@@ -122,7 +63,8 @@ public class PokemonController {
 	 */
 	@PostMapping({"/", ""})
 	public @ResponseBody ResponseEntity<String> createPokemon(@RequestBody Pokemon pokemon) {
-		return new ResponseEntity<String>(pokemon.getName() + " has been created ^^", HttpStatus.CREATED);
+		String msg = pokemonService.createPokemon(pokemon);
+		return new ResponseEntity<String>(msg, HttpStatus.CREATED);
 	}
 	
 	/** This method updates a specific pokemon
@@ -132,7 +74,8 @@ public class PokemonController {
 	 */
 	@PutMapping({"/", ""})
 	public @ResponseBody ResponseEntity<String> updatePokemon(@RequestBody Pokemon pokemon) {
-		return new ResponseEntity<String>(pokemon.getName() + " has been updated", HttpStatus.OK);
+		String msg = pokemonService.updatePokemon(pokemon);
+		return new ResponseEntity<String>(msg, HttpStatus.OK);
 	}
 	
 	/** This method deletes a specific pokemon
@@ -142,6 +85,7 @@ public class PokemonController {
 	 */
 	@DeleteMapping("/{id}")
 	public @ResponseBody ResponseEntity<String> deletePokemon(@PathVariable Long id) {
-		return new ResponseEntity<String>("The pokemon with the ID: " + id +" has been deleted", HttpStatus.NO_CONTENT);
+		String msg = pokemonService.deletePokemon(id);
+		return new ResponseEntity<String>(msg, HttpStatus.NO_CONTENT);
 	}	
 }
---
+++ src/main/java/ch/noseryoung/demo/pokemons/PokemonService.java
@@ -0,0 +1,99 @@
+package ch.noseryoung.demo.pokemons;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import ch.noseryoung.demo.abilities.Ability;
+import ch.noseryoung.demo.typings.Typing;
+
+@Service
+public class PokemonService {
+	public Pokemon getPokemon(Long id) {
+		ArrayList<Typing> resistantAgainst = new ArrayList<>();		
+		ArrayList<Typing> weakAgainst = new ArrayList<>();
+		ArrayList<Typing> strongAgainst = new ArrayList<>();
+		Typing typing = new Typing("Fighting", resistantAgainst, weakAgainst, strongAgainst);
+		ArrayList<Typing> typings = new ArrayList<>();
+		typings.add(typing);
+		
+		Ability innerFocus = new Ability("Inner focus", "This pokemon cannot flinch.");
+		Ability regenerator = new Ability("Regenerator", "Heals a bit when switched out.");
+		Ability reckless = new Ability("Reckless", "Moves that have recoil or crash damage deal more damage.");
+		ArrayList<Ability> abilities = new ArrayList<>();
+		abilities.add(innerFocus);
+		abilities.add(regenerator);
+		abilities.add(reckless);
+				
+		Pokemon pokemon = new Pokemon();
+		pokemon.setId(id);
+		pokemon.setHealth(65);
+		pokemon.setAttack(125);
+		pokemon.setDefense(60);
+		pokemon.setSpecialAttack(95);
+		pokemon.setSpecialDefense(60);
+		pokemon.setSpeed(105);
+		pokemon.setAbilities(abilities);
+		pokemon.setType(typings);
+		
+		return pokemon;
+	}
+	
+	public ArrayList<Pokemon> getAllPokemons() {
+		ArrayList<Typing> resistantAgainst = new ArrayList<>();
+		ArrayList<Typing> weakAgainst = new ArrayList<>();
+		ArrayList<Typing> strongAgainst = new ArrayList<>();
+		Typing typing = new Typing("Fighting", resistantAgainst, weakAgainst, strongAgainst);
+		ArrayList<Typing> typings = new ArrayList<>();
+		typings.add(typing);
+		
+		Ability innerFocus = new Ability("Inner focus", "This pokemon cannot flinch.");
+		Ability regenerator = new Ability("Regenerator", "Heals a bit when switched out.");
+		Ability reckless = new Ability("Reckless", "Moves that have recoil or crash damage deal more damage.");
+		ArrayList<Ability> abilities = new ArrayList<>();
+		abilities.add(innerFocus);
+		abilities.add(regenerator);
+		abilities.add(reckless);
+		
+		
+		Pokemon mienshao = new Pokemon();
+		mienshao.setId(Long.parseLong("620"));
+		mienshao.setHealth(65);
+		mienshao.setAttack(125);
+		mienshao.setDefense(60);
+		mienshao.setSpecialAttack(95);
+		mienshao.setSpecialDefense(60);
+		mienshao.setSpeed(105);
+		mienshao.setAbilities(abilities);
+		mienshao.setType(typings);
+		
+		Pokemon mienfoo = new Pokemon();
+		mienfoo.setId(Long.parseLong("619"));
+		mienfoo.setHealth(45);
+		mienfoo.setAttack(85);
+		mienfoo.setDefense(50);
+		mienfoo.setSpecialAttack(55);
+		mienfoo.setSpecialDefense(50);
+		mienfoo.setSpeed(65);
+		mienfoo.setAbilities(abilities);
+		mienfoo.setType(typings);
+		
+		
+		ArrayList<Pokemon> pokemons = new ArrayList<>();
+		pokemons.add(mienfoo);
+		pokemons.add(mienshao);
+		return pokemons;
+	}
+	
+	public String createPokemon(Pokemon pokemon) {
+		return pokemon.getName() + " has been created.";
+	}
+	
+	public String updatePokemon(Pokemon pokemon) {
+		return pokemon.getName() + " has been updated.";
+	}
+	
+	public String deletePokemon(Long id) {
+		return "The pokemon with the ID: " + id +" has been deleted.";
+	}
+}
