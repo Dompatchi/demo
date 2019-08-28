990d8f801bb5b18904c1d1984cb248af65411d5d
Jens H. S <jens.scheidmann@gmail.com>
2019-08-28 11:38:26 +0200

    no

--- src/main/java/ch/noseryoung/demo/persons/PersonController.java
+++ src/main/java/ch/noseryoung/demo/persons/PersonController.java
@@ -15,8 +15,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
+import io.swagger.annotations.AuthorizationScope;
+
 @RestController
 @RequestMapping("/person")
+@Api(
+		value = "UserController",
+		authorizations = { @Authorization(
+			value = "predefined authorization scheme",
+			scopes = {
+					@AuthorizationScope(
+						description = "description",
+						scope = "predeclared scope"
+					) }
+		) }
+	)
 public class PersonController {
 	private PersonService personService;
 
@@ -26,30 +44,79 @@ public class PersonController {
 		this.personService = personService;
 	}
 	
+	@ApiOperation(
+			value = "This endpoint returns the requested person",
+			response = Person.class
+		)
+		@ApiImplicitParams(
+			{ @ApiImplicitParam(
+				value = "Id of requested person",
+				required = true
+			) }
+		)
 	@GetMapping("/{id}")
 	public @ResponseBody ResponseEntity<Person> getPerson(@PathVariable Long id) {
 		Person person = personService.getPerson(id).get();
 		return new ResponseEntity<Person>(person, HttpStatus.OK);
 	}
 	
+	@ApiOperation(
+			value = "This endpoint returns all people",
+			response = Person.class
+		)
+		@ApiImplicitParams(
+			{ @ApiImplicitParam(
+				required = true
+			) }
+		)
 	@GetMapping({"", "/"})
 	public @ResponseBody ResponseEntity<List<Person>> getAllPersons() {
 		List<Person> list = personService.getAllPersons();
 		return new ResponseEntity<List<Person>>(list, HttpStatus.OK);
 	}
 	
+	@ApiOperation(
+			value = "This endpoint creates the requested person",
+			response = Person.class
+		)
+		@ApiImplicitParams(
+			{ @ApiImplicitParam(
+				value = "Requested person",
+				required = true
+			) }
+		)
 	@PostMapping({"", "/"})
 	public @ResponseBody ResponseEntity<String> createPerson(@RequestBody Person person) {
 		personService.createPerson(person);
 		return new ResponseEntity<String>("Person created", HttpStatus.CREATED);
 	}
 	
+	@ApiOperation(
+			value = "This endpoint updates the requested person",
+			response = Person.class
+		)
+		@ApiImplicitParams(
+			{ @ApiImplicitParam(
+				value = "Id of requested person",
+				required = true
+			) }
+		)
 	@PutMapping({"", "/"})
 	public @ResponseBody ResponseEntity<String> updatePerson(@RequestBody Person person) {
 		personService.updatePerson(person);
 		return new ResponseEntity<String>("Person updated", HttpStatus.CREATED);
 	}
 	
+	@ApiOperation(
+			value = "This endpoint deletes the requested person",
+			response = Person.class
+		)
+		@ApiImplicitParams(
+			{ @ApiImplicitParam(
+				value = "Id of requested person",
+				required = true
+			) }
+		)
 	@DeleteMapping("/{id}")
 	public @ResponseBody ResponseEntity<String> deletePerson(@PathVariable Long id) {
 		personService.deletePerson(id);
